a975d892ca3d343537d09c51a50f9903b5f5221c
jared w <jaredtimothywillis@gmail.com>
2018-08-07 01:52:03 -0700

    create from command line

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -2,6 +2,17 @@ class Api::ItemsController < ApiController
   before_action :authenticated?
 
   def create
+    item = current_user.lists.find(params[:list_id]).items.build(item_params)
+    if item.save
+      render json: item
+    else
+      render json: { errors: item.errors.full_messages },
+      status: :unprocessable_entity
+    end
   end
 
+  private
+  def item_params
+    params.require(:item).permit(:name, :status)
+  end
 end
--- app/controllers/api/lists_controller.rb
+++ app/controllers/api/lists_controller.rb
@@ -2,6 +2,17 @@ class Api::ListsController < ApiController
   before_action :authenticated?
 
   def create
+    list = current_user.lists.build(list_params)
+    if list.save
+      render json: list
+    else
+      render json: { errors: list.errors.full_messages },
+      status: :unprocessable_entity
+    end
   end
 
+  private
+  def list_params
+    params.require(:list).permit(:title, :permissions)
+  end
 end
